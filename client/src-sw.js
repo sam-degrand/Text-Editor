3b77caa3f45dba94336038085445fcb5418bbc5b
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-08-21 19:22:09 -0400

    implemented asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+// Cache static assets (e.g., CSS, JS, images) using a StaleWhileRevalidate strategy  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'static-assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 100, // Limit the cache size
+        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache assets for 7 days
+      }),
+    ],
+  })
+);
