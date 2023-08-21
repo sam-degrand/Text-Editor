d6cf4ff9176656d2240ed02ec6056e089fecc223
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-08-21 19:09:16 -0400

    set up putdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  try {
+    console.log('PUT to the database');
+    const contactDb = await initdb();
+    const tx = contactDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ value: content });
+    await request;
+    console.log('data saved to the database');
+  } catch (error) {
+    console.error('Error putting data into the database:', error);
+  }
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
